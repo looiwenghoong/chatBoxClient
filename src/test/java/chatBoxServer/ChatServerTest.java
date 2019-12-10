312ce2300a93aecdb3304b9c0b0a358cdad5ca01
Looi <https://github.com/looiwenghoong/WallCom.git>
2019-12-10 22:49:31 +0800

    Testing on junit

---
+++ src/test/java/chatBoxServer/ChatServerTest.java
@@ -0,0 +1,13 @@
+package chatBoxServer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChatServerTest {
+
+    @Test
+    public void testRun1() {
+        System.out.println("Hello");
+    }
+}
